da6bedd24fdaeb76146253c7463106328c47f38d
CJ Vaughn <cjvaughn@rocketmail.com>
2019-08-18 11:11:28 -0700

    Add project files.

---
+++ Calculator.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.btnZero = New System.Windows.Forms.Button()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnSubtract = New System.Windows.Forms.Button()
+        Me.btnMultiply = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.textInput = New System.Windows.Forms.RichTextBox()
+        Me.btnEquals = New System.Windows.Forms.Button()
+        Me.resultLabel = New System.Windows.Forms.Label()
+        Me.prevResultLabel = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnOne
+        '
+        Me.btnOne.Location = New System.Drawing.Point(12, 178)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(75, 23)
+        Me.btnOne.TabIndex = 0
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(93, 178)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(75, 23)
+        Me.btnTwo.TabIndex = 1
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Location = New System.Drawing.Point(174, 178)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(75, 23)
+        Me.btnThree.TabIndex = 2
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Location = New System.Drawing.Point(12, 207)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(75, 23)
+        Me.btnFour.TabIndex = 3
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Location = New System.Drawing.Point(93, 207)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(75, 23)
+        Me.btnFive.TabIndex = 4
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Location = New System.Drawing.Point(174, 207)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(75, 23)
+        Me.btnSix.TabIndex = 5
+        Me.btnSix.Text = "6"
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnSeven
+        '
+        Me.btnSeven.Location = New System.Drawing.Point(12, 236)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(75, 23)
+        Me.btnSeven.TabIndex = 6
+        Me.btnSeven.Text = "7"
+        Me.btnSeven.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Location = New System.Drawing.Point(93, 236)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(75, 23)
+        Me.btnEight.TabIndex = 7
+        Me.btnEight.Text = "8"
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Location = New System.Drawing.Point(174, 236)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(75, 23)
+        Me.btnNine.TabIndex = 8
+        Me.btnNine.Text = "9"
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'btnZero
+        '
+        Me.btnZero.Location = New System.Drawing.Point(93, 265)
+        Me.btnZero.Name = "btnZero"
+        Me.btnZero.Size = New System.Drawing.Size(75, 23)
+        Me.btnZero.TabIndex = 9
+        Me.btnZero.Text = "0"
+        Me.btnZero.UseVisualStyleBackColor = True
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(289, 178)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(75, 23)
+        Me.btnAdd.TabIndex = 10
+        Me.btnAdd.Text = "+"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnSubtract
+        '
+        Me.btnSubtract.Location = New System.Drawing.Point(289, 207)
+        Me.btnSubtract.Name = "btnSubtract"
+        Me.btnSubtract.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubtract.TabIndex = 11
+        Me.btnSubtract.Text = "-"
+        Me.btnSubtract.UseVisualStyleBackColor = True
+        '
+        'btnMultiply
+        '
+        Me.btnMultiply.Location = New System.Drawing.Point(289, 236)
+        Me.btnMultiply.Name = "btnMultiply"
+        Me.btnMultiply.Size = New System.Drawing.Size(75, 23)
+        Me.btnMultiply.TabIndex = 12
+        Me.btnMultiply.Text = "*"
+        Me.btnMultiply.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.BackColor = System.Drawing.Color.Transparent
+        Me.btnDivide.Location = New System.Drawing.Point(289, 265)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(75, 23)
+        Me.btnDivide.TabIndex = 13
+        Me.btnDivide.Text = "/"
+        Me.btnDivide.UseVisualStyleBackColor = False
+        '
+        'textInput
+        '
+        Me.textInput.Font = New System.Drawing.Font("Trebuchet MS", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.textInput.Location = New System.Drawing.Point(12, 38)
+        Me.textInput.Name = "textInput"
+        Me.textInput.Size = New System.Drawing.Size(352, 96)
+        Me.textInput.TabIndex = 14
+        Me.textInput.Text = ""
+        '
+        'btnEquals
+        '
+        Me.btnEquals.Location = New System.Drawing.Point(289, 295)
+        Me.btnEquals.Name = "btnEquals"
+        Me.btnEquals.Size = New System.Drawing.Size(75, 23)
+        Me.btnEquals.TabIndex = 15
+        Me.btnEquals.Text = "="
+        Me.btnEquals.UseVisualStyleBackColor = True
+        '
+        'resultLabel
+        '
+        Me.resultLabel.AutoSize = True
+        Me.resultLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.resultLabel.Location = New System.Drawing.Point(24, 397)
+        Me.resultLabel.Name = "resultLabel"
+        Me.resultLabel.Size = New System.Drawing.Size(0, 20)
+        Me.resultLabel.TabIndex = 16
+        '
+        'prevResultLabel
+        '
+        Me.prevResultLabel.AutoSize = True
+        Me.prevResultLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.prevResultLabel.Location = New System.Drawing.Point(265, 397)
+        Me.prevResultLabel.Name = "prevResultLabel"
+        Me.prevResultLabel.Size = New System.Drawing.Size(0, 20)
+        Me.prevResultLabel.TabIndex = 17
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(25, 357)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Result"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(266, 356)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(86, 13)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Previous Results"
+        '
+        'Calculator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.ClientSize = New System.Drawing.Size(400, 480)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.prevResultLabel)
+        Me.Controls.Add(Me.resultLabel)
+        Me.Controls.Add(Me.btnEquals)
+        Me.Controls.Add(Me.textInput)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnMultiply)
+        Me.Controls.Add(Me.btnSubtract)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.btnZero)
+        Me.Controls.Add(Me.btnNine)
+        Me.Controls.Add(Me.btnEight)
+        Me.Controls.Add(Me.btnSeven)
+        Me.Controls.Add(Me.btnSix)
+        Me.Controls.Add(Me.btnFive)
+        Me.Controls.Add(Me.btnFour)
+        Me.Controls.Add(Me.btnThree)
+        Me.Controls.Add(Me.btnTwo)
+        Me.Controls.Add(Me.btnOne)
+        Me.ForeColor = System.Drawing.SystemColors.MenuText
+        Me.Name = "Calculator"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents btnZero As Button
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnSubtract As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents textInput As RichTextBox
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents resultLabel As Label
+    Friend WithEvents prevResultLabel As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Calculator.vb
@@ -0,0 +1,167 @@
+﻿
+Public Class Calculator
+    Dim leftNumber As String = ""
+    Dim rightNumber As String = ""
+    Dim numOperator As String = ""
+    Dim resultCounter As Integer = 0
+    Dim cjCalc As New myCalculator(leftNumber, rightNumber, numOperator)
+
+    Private Sub btnZero_Click(sender As Object, e As EventArgs) _
+        Handles btnZero.Click
+        textInput.Text = textInput.Text & btnZero.Text
+    End Sub
+    Private Sub btnOne_Click(sender As Object, e As EventArgs) _
+        Handles btnOne.Click
+        textInput.Text = textInput.Text & btnOne.Text
+    End Sub
+    Private Sub btnTwo_Click(sender As Object, e As EventArgs) _
+        Handles btnTwo.Click
+        textInput.Text = textInput.Text & btnTwo.Text
+    End Sub
+    Private Sub btnThree_Click(sender As Object, e As EventArgs) _
+        Handles btnThree.Click
+        textInput.Text = textInput.Text & btnThree.Text
+    End Sub
+    Private Sub btnFour_Click(sender As Object, e As EventArgs) _
+        Handles btnFour.Click
+        textInput.Text = textInput.Text & btnFour.Text
+    End Sub
+    Private Sub btnFive_Click(sender As Object, e As EventArgs) _
+        Handles btnFive.Click
+        textInput.Text = textInput.Text & btnFive.Text
+    End Sub
+    Private Sub btnSix_Click(sender As Object, e As EventArgs) _
+        Handles btnSix.Click
+        textInput.Text = textInput.Text & btnSix.Text
+    End Sub
+    Private Sub btnSeven_Click(sender As Object, e As EventArgs) _
+        Handles btnSeven.Click
+        textInput.Text = textInput.Text & btnSeven.Text
+    End Sub
+    Private Sub btnEight_Click(sender As Object, e As EventArgs) _
+        Handles btnEight.Click
+        textInput.Text = textInput.Text & btnEight.Text
+    End Sub
+    Private Sub btnNine_Click(sender As Object, e As EventArgs) _
+        Handles btnNine.Click
+        textInput.Text = textInput.Text & btnNine.Text
+    End Sub
+
+
+    'Be sure to account for no inputs and put up an error message box if no input is provided
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        If (Not (textInput.Text Is "") And cjCalc.lNumber Is "") Then
+            'create the setLeft function and replace it here
+            cjCalc.SetLeft(textInput.Text)
+            cjCalc.SetOperator(btnAdd.Text)
+            textInput.Text = ""
+        End If
+
+    End Sub
+
+    Private Sub BtnSubtract_Click(sender As Object, e As EventArgs) Handles btnSubtract.Click
+        If (Not (textInput.Text Is "") And cjCalc.lNumber Is "") Then
+            'create the setLeft function and replace it here
+            cjCalc.SetLeft(textInput.Text)
+            cjCalc.SetOperator(btnSubtract.Text)
+            textInput.Text = ""
+        End If
+    End Sub
+
+    Private Sub BtnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
+        If (Not (textInput.Text Is "") And cjCalc.lNumber Is "") Then
+            'create the setLeft function and replace it here
+            cjCalc.SetLeft(textInput.Text)
+            cjCalc.SetOperator(btnMultiply.Text)
+            textInput.Text = ""
+        End If
+    End Sub
+
+    Private Sub BtnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        If (Not (textInput.Text Is "") And cjCalc.lNumber Is "") Then
+            'create the setLeft function and replace it here
+            cjCalc.SetLeft(textInput.Text)
+            cjCalc.SetOperator(btnDivide.Text)
+            textInput.Text = ""
+        End If
+    End Sub
+
+
+    Public Sub BtnEquals_Click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        If (Not (textInput.Text Is "") And cjCalc.lNumber) Then
+            cjCalc.SetRight(textInput.Text)
+        End If
+        If (Not (cjCalc.lNumber Is "") And Not (cjCalc.rNumber Is "") And Not (cjCalc.numOper Is "")) Then
+            cjCalc.calculate()
+            resultLabel.Text = cjCalc.lNumber + " " + cjCalc.numOper + " " + cjCalc.rNumber + " = " + cjCalc.getResult().ToString()
+            If (cjCalc.Results.Count > 1) Then
+                resultCounter += 1
+                prevResultLabel.Text = cjCalc.getPreviousResult(resultCounter - 1)
+            End If
+            cjCalc.SetLeft("")
+            cjCalc.SetRight("")
+
+        End If
+
+        textInput.Text = ""
+
+    End Sub
+
+
+End Class
+
+Public Class myCalculator
+
+    Public lNumber As String
+    Public rNumber As String
+    Public numOper As String
+    Public Results As New List(Of Double)
+
+    Public Sub New(ByVal leftNum As String, rightNum As String, oper As String)
+
+        lNumber = leftNum
+        rNumber = rightNum
+        numOper = oper
+
+    End Sub
+
+    Public Sub SetLeft(ByVal leftNum As String)
+        lNumber = leftNum
+    End Sub
+    Public Sub SetRight(ByVal rightNum As String)
+        rNumber = rightNum
+    End Sub
+    Public Sub SetOperator(ByVal oper As String)
+        numOper = oper
+    End Sub
+
+    Public Sub calculate()
+
+        If (numOper Is "+") Then
+            Results.Add(Double.Parse(lNumber) + Double.Parse(rNumber))
+        ElseIf (numOper Is "-") Then
+            Results.Add(Double.Parse(lNumber) - Double.Parse(rNumber))
+        ElseIf (numOper Is "*") Then
+            Results.Add(Double.Parse(lNumber) * Double.Parse(rNumber))
+        ElseIf (numOper Is "/") Then
+            Results.Add(Double.Parse(lNumber) / Double.Parse(rNumber))
+        End If
+
+    End Sub
+
+    Public Function getResult()
+        If (Results.Count > 0) Then
+            Return Results(Results.Count - 1)
+        Else
+            Return Results(0)
+        End If
+
+    End Function
+
+    Public Function getPreviousResult(ByVal index As Integer)
+        Return Results(index)
+    End Function
+
+
+End Class
+
